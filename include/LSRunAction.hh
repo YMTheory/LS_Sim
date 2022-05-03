3d4780f37bebc8e5d0d5799ed961711c388bf21d
YMTheory <932899789@qq.com>
2022-05-03 15:29:30 +0800

    Set random seed automatically each run

--- include/LSRunAction.hh
+++ include/LSRunAction.hh
@@ -14,7 +14,15 @@ class LSRunAction : public G4UserRunAction
 
         virtual void BeginOfRunAction(const G4Run*);
         virtual void   EndOfRunAction(const G4Run*);
+
+        void SetRndmFreq(G4int val) { fSaveRndm = val; }
+        G4int GetRndmFreq()         { return fSaveRndm;}
+
+        inline void SetAutoSeed(const G4bool val) {fAutoSeed = val;}
         
+    private:
+        G4int  fSaveRndm;
+        G4bool fAutoSeed;
 
 };
 
--- src/LSRunAction.cc
+++ src/LSRunAction.cc
@@ -1,8 +1,10 @@
 #include "LSRunAction.hh"
 #include "LSAnalysisManager.hh"
+#include "Randomize.hh"
+#include <ctime>
 
 LSRunAction::LSRunAction()
-: G4UserRunAction()
+: G4UserRunAction(), fSaveRndm(0), fAutoSeed(true)
 {;}
 
 
@@ -15,6 +17,28 @@ void LSRunAction::BeginOfRunAction(const G4Run*)
     LSAnalysisManager* analysis = LSAnalysisManager::getInstance();
     analysis->book();
 
+    if (fAutoSeed) {
+     // automatic (time-based) random seeds for each run
+     G4cout << "*******************" << G4endl;
+     G4cout << "*** AUTOSEED ON ***" << G4endl;
+     G4cout << "*******************" << G4endl;
+     long seeds[2];
+     time_t systime = time(NULL);
+     seeds[0] = (long) systime;
+     seeds[1] = (long) (systime*G4UniformRand());
+     G4Random::setTheSeeds(seeds);
+     G4Random::showEngineStatus();
+  } else {
+     G4Random::showEngineStatus();
+  }
+
+  if (fSaveRndm > 0)
+  {
+     std::ostringstream os;
+     os<<"beginOfRun_"<<fSaveRndm<<".rndm";
+     G4Random::saveEngineStatus(os.str().c_str());
+  }
+
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -23,6 +47,13 @@ void LSRunAction::EndOfRunAction(const G4Run* )
 {
     LSAnalysisManager* analysis = LSAnalysisManager::getInstance();
     analysis->finish();
+
+    if (fSaveRndm == 1) {
+     G4Random::showEngineStatus();
+     std::ostringstream os;
+     os<<"endOfRun_"<<fSaveRndm<<".rndm";
+     G4Random::saveEngineStatus(os.str().c_str());
+  }
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
